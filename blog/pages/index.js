86c38f83e9bfe2445de52e1915b100a719eca117
theone <xuguangyue@dtrobo.com>
2019-10-21 16:59:19 +0800

    前台开发环境搭建

---
+++ blog/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+import {Button} from 'antd'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <Button>按钮test</Button>
+  </div>
+)
+
+export default Home
