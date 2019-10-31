3ea760f37cf4373e0c14045d067208eb22a0ee04
theone <xuguangyue@dtrobo.com>
2019-10-31 15:24:35 +0800

    react-markdown使用 => 解析

--- blog/pages/detailsed.js
+++ blog/pages/detailsed.js
@@ -6,42 +6,85 @@ import Advert from '../components/Advert'
 import Author from '../components/Author'
 import Footer from '../components/Footer'
 import '../static/style/components/detailsed.css'
-const Detailsed = () => (
-  <div>
-    <Head>
-      <title>Detailsed</title>
-    </Head>
-    <Header/>
-    <Row className="comm-main" type="flex" justify="center">
-      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-        <div className="bread-div">
-          <Breadcrumb>
-            <Breadcrumb.Item>
-              <a href="/">首页</a>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              视频
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>XXX</Breadcrumb.Item>
-          </Breadcrumb>
-        </div>
-        <div className="detailed-title">
-            视频标题是：aaabbadasdad, 作者徐老作
-        </div>
-        <div className="list-icon center">
-          <span><Icon type="calendar" style={{color: '#909399'}}/> 2019-10-31</span>
-          <span><Icon type="folder" style={{color: '#409eff'}}/> 视频</span>
-          <span><Icon type="fire" style={{color: '#F56C6C'}}/> 6666人</span>
-        </div>
-        <div className="detailed-content">
-            内容是：：：：：
-        </div>
-      </Col>
-      <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-        right
-      </Col>
-    </Row>
-  </div>
-)
+
+import ReactMarkDown  from 'react-markdown'
+const Detailsed = () => {
+  let markdown='# P01:课程介绍和环境搭建\n' +
+  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+   '**这是加粗的文字**\n\n' +
+  '*这是倾斜的文字*`\n\n' +
+  '***这是斜体加粗的文字***\n\n' +
+  '~~这是加删除线的文字~~ \n\n'+
+  '\`console.log(111)\` \n\n'+
+  '# p02:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '#5 p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '``` var a=11; ```'
+  return (
+    <div>
+      <Head>
+        <title>Detailsed</title>
+      </Head>
+      <Header/>
+      <Row className="comm-main" type="flex" justify="center">
+        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+          <div className="bread-div">
+            <Breadcrumb>
+              <Breadcrumb.Item>
+                <a href="/">首页</a>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                视频
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>XXX</Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+          <div className="detailed-title">
+              视频标题是：aaabbadasdad, 作者：徐老作
+          </div>
+          <div className="list-icon center">
+            <span><Icon type="calendar" style={{color: '#909399'}}/> 2019-10-31</span>
+            <span><Icon type="folder" style={{color: '#409eff'}}/> 视频</span>
+            <span><Icon type="fire" style={{color: '#F56C6C'}}/> 6666人</span>
+          </div>
+          <div className="detailed-content">
+              <ReactMarkDown 
+                source={markdown}
+                escapeHtml={false}
+              />
+          </div>
+        </Col>
+        <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+          <Author/>
+          <Advert/>
+        </Col>
+      </Row>
+      <Footer/>
+    </div>
+  )
+}
 
 export default Detailsed
