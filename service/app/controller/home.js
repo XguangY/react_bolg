335b5ce90792058d2312a7b4913670c8359845c6
theone <xuguangyue@dtrobo.com>
2020-01-14 16:38:54 +0800

    test

--- service/app/controller/home.js
+++ service/app/controller/home.js
@@ -7,6 +7,10 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
+  async list() {
+    const { ctx } = this;
+    ctx.body = 'hi, list';
+  }
 }
 
 module.exports = HomeController;
