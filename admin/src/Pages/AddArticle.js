aabb56881602da058bf33b63f32741a391279a7c
theone <xuguangyue@dtrobo.com>
2020-02-20 15:55:58 +0800

    文章实现MD编写

--- admin/src/Pages/AddArticle.js
+++ admin/src/Pages/AddArticle.js
@@ -6,6 +6,40 @@ const { Option } = Select
 const { TextArea } = Input
 
 function AddArticle() {
+    const [articleId, setArticleId] = useState(0)  // 文章的ID，如果是0说明是新增加，如果不是0，说明是修改
+    const [articleTitle, setArticleTitle] = useState('')   //文章标题
+    const [articleContent, setArticleContent] = useState('')  //markdown的编辑内容
+    const [markdownContent, setMarkdownContent] = useState('预览内容') //html内容
+    const [introducemd, setIntroducemd] = useState()            //简介的markdown内容
+    const [introducehtml, setIntroducehtml] = useState('等待编辑') //简介的html内容
+    const [showDate, setShowDate] = useState()   //发布日期
+    const [updateDate, setUpdateDate] = useState() //修改日志的日期
+    const [typeInfo, setTypeInfo] = useState([]) // 文章类别信息
+    const [selectedType, setSelectType] = useState(1) //选择的文章类别
+    const renderer = new marked.Renderer();
+    marked.setOptions({
+        renderer: renderer,
+        gfm: true,
+        pedantic: false,
+        sanitize: false,
+        tables: true,
+        breaks: false,
+        smartLists: true,
+        smartypants: false,
+    });
+
+    const changeContent = (e) => {
+        setArticleContent(e.target.value)
+        let html = marked(e.target.value)
+        setMarkdownContent(html)
+    }
+
+    const changeIntroduce = (e) => {
+        setIntroducemd(e.target.value)
+        let html = marked(e.target.value)
+        setIntroducehtml(html)
+    }
+
     return (
         <div>
             <Row gutter={5}>
@@ -25,16 +59,18 @@ function AddArticle() {
                     <Row gutter={10} >
                         <Col span={12}>
                             <TextArea
+                                value={articleContent}
                                 className="markdown-content"
                                 rows={35}
                                 placeholder="文章内容"
+                                onChange={changeContent}
+                                onPressEnter={changeContent}
                             />
                         </Col>
                         <Col span={12}>
                             <div
-                                className="show-html">
-
-                            </div>
+                                className="show-html"
+                                dangerouslySetInnerHTML={{ __html: markdownContent }} />
 
                         </Col>
                     </Row>
@@ -49,11 +85,14 @@ function AddArticle() {
                         <Col span={24}>
                             <br />
                             <TextArea
+                                value={introducemd}
                                 rows={4}
                                 placeholder="文章简介"
+                                onChange={changeIntroduce}
+                                onPressEnter={changeIntroduce}
                             />
                             <br /><br />
-                            <div className="introduce-html"></div>
+                            <div className="introduce-html" dangerouslySetInnerHTML={{ __html: '文章简介：' + introducehtml }}></div>
                         </Col>
                         <Col span={12}>
                             <div className="date-select">
